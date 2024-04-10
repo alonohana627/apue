ffb2360fc530f50d4a301101ed6efc638a6c9e36
alon <alon4dota2@gmail.com>
2024-04-10 20:28:40 +0300

    Type?2

--- src/shell.c
+++ src/shell.c
@@ -5,33 +5,5 @@
 #include <string.h>
 
 void run_shell(void) {
-    char buf[MAXLINE_SIZE];
-    pid_t pid;
-    int status;
-
-    printf("%% ");
-
-    while (fgets(buf, MAXLINE_SIZE, stdin) != NULL) {
-        if (buf[strnlen(buf, MAXLINE_SIZE) - 1] == '\n') {
-            buf[strnlen(buf, MAXLINE_SIZE) - 1] = 0;
-        }
-
-        pid = fork();
-        if (pid < 0) {
-            printf("Fork Error\n");
-            return;
-        } else if (pid == 0) { /* child */
-            safe_string_arr_t *splited_args = split_by_space(buf);
-            printf("Couldn'st execute %s\n", buf);
-            execvp(splited_args->strings[0], splited_args->strings);
-            return;
-        }
-
-        pid = waitpid(pid, &status, 0);
-        if (pid < 0) {
-            printf("WaitPID Error\n");
-            return;
-        }
-        printf("%% ");
-    }
+    
 }
